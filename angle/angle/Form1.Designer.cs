96aebc7bd0b2ea5410363e8710125841ecd4e298
roybogin <roybog00@gmail.com>
2019-11-28 22:46:56 +0200

    Add files via upload

---
+++ angle/angle/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace angle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(48, 104);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(112, 26);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(73, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "angle";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(513, 614);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 45);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Draw Triangle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(795, 141);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(206, 212);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Location = new System.Drawing.Point(336, 141);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(149, 79);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(1169, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "mass";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1144, 104);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(112, 26);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(1190, 184);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "μ";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(1144, 212);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(112, 26);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(1017, 494);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "ΣFx =";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(1081, 494);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 25);
+            this.label5.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(1081, 542);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 25);
+            this.label6.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(1037, 542);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 25);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "a =";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1350, 742);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ angle/angle/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace angle
+{
+    public partial class Form1 : Form
+    {
+        int y = 350;    //top of the triangle
+        double angle = 0;
+        double rad = 0;
+        int width = 149;    //size of the box
+        int height = 79;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static Bitmap RotateImage(Bitmap bmp, float angle, Color bkColor)    //rotate an image to fit the angle
+        {
+            angle = angle % 360;
+            if (angle > 180)
+                angle -= 360;
+
+            System.Drawing.Imaging.PixelFormat pf = default(System.Drawing.Imaging.PixelFormat);
+            if (bkColor == Color.Transparent)
+            {
+                pf = System.Drawing.Imaging.PixelFormat.Format32bppArgb;
+            }
+            else
+            {
+                pf = bmp.PixelFormat;
+            }
+
+            float sin = (float)Math.Abs(Math.Sin(angle * Math.PI / 180.0)); // this function takes radians
+            float cos = (float)Math.Abs(Math.Cos(angle * Math.PI / 180.0)); // this one too
+            float newImgWidth = sin * bmp.Height + cos * bmp.Width;
+            float newImgHeight = sin * bmp.Width + cos * bmp.Height;
+            float originX = 0f;
+            float originY = 0f;
+
+            if (angle > 0)
+            {
+                if (angle <= 90)
+                    originX = sin * bmp.Height;
+                else
+                {
+                    originX = newImgWidth;
+                    originY = newImgHeight - sin * bmp.Width;
+                }
+            }
+            else
+            {
+                if (angle >= -90)
+                    originY = sin * bmp.Width;
+                else
+                {
+                    originX = newImgWidth - sin * bmp.Height;
+                    originY = newImgHeight;
+                }
+            }
+
+            Bitmap newImg = new Bitmap((int)newImgWidth, (int)newImgHeight, pf);
+            Graphics g = Graphics.FromImage(newImg);
+            g.Clear(bkColor);
+            g.TranslateTransform(originX, originY); // offset the origin to our calculated values
+            g.RotateTransform(angle); // set up rotate
+            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBilinear;
+            g.DrawImageUnscaled(bmp, 0, 0); // draw the image at 0, 0
+            g.Dispose();
+
+            return newImg;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        { 
+            Graphics gr = e.Graphics;
+            Pen p = new Pen(SystemColors.Highlight, 10);
+            SolidBrush b = new SolidBrush(SystemColors.Highlight);
+            Point[] point = { new Point(200, 350), new Point(500, 350), new Point(500, y) };
+            gr.DrawPolygon(p, point);
+            gr.FillPolygon(b, point);
+            b.Dispose();
+            p.Dispose();
+            gr.Dispose();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double mass;    //mass of object
+            double coef;    //coefficient of friction
+            try
+            {
+                mass = Convert.ToDouble(textBox2.Text);
+                coef = Convert.ToDouble(textBox3.Text);
+                angle = Convert.ToDouble(textBox1.Text);
+            }
+            catch
+            {
+                MessageBox.Show("The conversion of the inputs to numbers didn't work properly", "Problem Detected");
+                return;
+            }
+
+            double acc;
+            if (mass <= 0 || coef < 0 || angle < 0 || angle >= 90)
+            {
+                MessageBox.Show("At least one of the inputs doesn't make sense", "Problem Detected");
+                return;
+            }
+            pictureBox1.Load("../../images/system.png");    //load the x and y arrows
+            pictureBox2.Load("../../images/object.png");    //load the object
+            pictureBox1.Image = RotateImage(new Bitmap(pictureBox1.Image), 360 - (float)angle, Color.Transparent);  //Rotate the images and make the background transparent
+            pictureBox2.Image = RotateImage(new Bitmap(pictureBox2.Image), 360 - (float)angle, Color.Transparent);
+            rad = angle * Math.PI / 180.0;
+            y = (int)(350 - Math.Tan(rad) * 300);
+            //calculate new x and y for the box (top left transparent pixel)
+            int box_x = (int)(350 - (width * Math.Cos(rad) / 2.0) - (height * Math.Sin(rad)));
+            int box_y = (int)(((350 + y) / 2.0) - (width * Math.Sin(rad) / 2.0) - (height * Math.Cos(rad)));
+            pictureBox2.Location = new Point(box_x, box_y); 
+            this.Invalidate();
+            double totalforce = mass * 10 * Math.Sin(rad) - mass * 10 * Math.Cos(rad) * coef;   //calculate force on box
+            totalforce = ((totalforce + Math.Abs(totalforce)) / 2.0);   //if the force is negative, turn it to 0
+            acc = totalforce / mass;    //Newton's second law
+            label5.Text = totalforce + " N";
+            label6.Text = acc + "  m/s\xB2";
+        }
+    }
+}
